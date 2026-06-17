9690659617fe84eb4f186af9edf3da3bcfd144a4
artioli90-art <artioli90@gmail.com>
2026-06-17 10:14:57 +0200

    creazione tabella nomi e immagini

---
+++ zoom.js
@@ -0,0 +1,38 @@
+const zoomCharacters = [
+  {
+    image: "images/zoom/brienne1.png",
+    answer: "Brienne di Tarth"
+  },
+  {
+    image: "images/zoom/daenerys1.png",
+    answer: "Daenerys Targaryen"
+  },
+  {
+    image: "images/zoom/jaime1.png",
+    answer: "Jaime Lannister"
+  },
+  {
+    image: "images/zoom/jon1.png",
+    answer: "Jon Snow"
+  },
+  {
+    image: "images/zoom/ned1.png",
+    answer: "Eddard Stark"
+  },
+  {
+    image: "images/zoom/pod1.png",
+    answer: "Podrick Payne"
+  },
+  {
+    image: "images/zoom/robb1.png",
+    answer: "Robb Stark"
+    },
+  {
+    image: "images/zoom/tyrion1.png",
+    answer: "Tyrion Lannister"
+    },
+  {
+    image: "images/zoom/tywin.png",
+    answer: "Tywin Lannister"
+  }
+];
