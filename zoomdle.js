c0ecab73bfe4345a02ae7eab39242e0ef322ed94
artioli90-art <artioli90@gmail.com>
2026-06-17 08:40:22 +0200

    prova immagine nell'html

--- zoomdle.js
+++ zoomdle.js
@@ -1,6 +1,27 @@
-const solution = "Stark Banner"; // esempio
+const images = [
+  {
+    image: "images/daenerys1.png",
+    answer: "Jon Snow"
+  },
+];
 
 let errors = 0;
+
+const today = new Date();
+
+const seed =
+    today.getFullYear() * 10000 +
+    (today.getMonth() + 1) * 100 +
+    today.getDate();
+
+const index = getDailyIndex(seed, images.length);
+
+const currentCharacter = images[index];
+
+wrapper.style.backgroundImage =
+  `url("${currentCharacter.image}")`;
+
+const solution = currentCharacter.answer;
 
 const wrapper = document.getElementById("imageWrapper");
 const input = document.getElementById("guessInput");
@@ -17,29 +38,12 @@ function setZoom(force = null) {
   else if (level === 2) wrapper.classList.add("z200");
   else if (level === 3) wrapper.classList.add("z150");
   else wrapper.classList.add("z100");
-}
-//VECCHIA //function setZoom() {
-  //wrapper.classList.remove("z400","z300","z200","z150","z100");
-
- // if (errors === 0) wrapper.classList.add("z400");
- // else if (errors === 1) wrapper.classList.add("z300");
- // else if (errors === 2) wrapper.classList.add("z200");
- // else if (errors === 3) wrapper.classList.add("z150");
- // else wrapper.classList.add("z100");
-//}
 
 function check() {
   const guess = input.value.trim();
 
   if (!guess) return;
   
-//VECCHIA
-//  if (guess.toLowerCase() === solution.toLowerCase()) {
-//    feedback.textContent = "✔ Corretto!";
-//    feedback.style.color = "green";
-//    return;
-//  }
-
   //NUOVA
   if (guess.toLowerCase() === solution.toLowerCase()) {
   feedback.textContent = "✔ Corretto!";
