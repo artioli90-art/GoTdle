2843b4c57964385f3ce01a813d26499b26af2c5c
artioli90-art <artioli90@gmail.com>
2026-06-18 08:47:21 +0200

    aggiunte tante voci alla lista nomi e immagini

--- zoomdle.js
+++ zoomdle.js
@@ -32,8 +32,288 @@ const images = [
     answer: "Robb Stark"
     },
   {
-    image: "images/zoom/tywin.png",
+    image: "images/zoom/tywin1.png",
     answer: "Tywin Lannister"
+  },
+  {
+    image: "images/zoom/aemon1.png",
+    answer: "Aemon Targaryen"
+  },
+  {
+    image: "images/zoom/alliser1.png",
+    answer: "Alliser Thorne"
+  },
+  {
+    image: "images/zoom/arya1.png",
+    answer: "Arya Stark"
+  },
+  {
+    image: "images/zoom/baelish1.png",
+    answer: "Petyr Baelish"
+  },
+  {
+    image: "images/zoom/benjen1.png",
+    answer: "Benjen Stark"
+  },
+  {
+    image: "images/zoom/barristan1.png",
+    answer: "Barristan Selmy"
+  },
+  {
+    image: "images/zoom/beric1.png",
+    answer: "Beric Dondarrion"
+  },
+  {
+    image: "images/zoom/bran1.png",
+    answer: "Bran Stark"
+  },
+  {
+    image: "images/zoom/bronn1.png",
+    answer: "Bronn delle Acque Nere"
+  },
+  {
+    image: "images/zoom/catelyn1.png",
+    answer: "Catelyn Stark"
+  },
+  {
+    image: "images/zoom/cersei1.png",
+    answer: "Cersei Lannister"
+  },
+  {
+    image: "images/zoom/crow1.png",
+    answer: "Corvo Con Tre Occhi"
+  },
+  {
+    image: "images/zoom/davos1.png",
+    answer: "Davos Seaworth"
+  },
+  {
+    image: "images/zoom/drogo1.png",
+    answer: "Khal Drogo"
+  },
+  {
+    image: "images/zoom/edd1.png",
+    answer: "Edd l'Addolorato"
+  },
+  {
+    image: "images/zoom/ellaria1.png",
+    answer: "Ellaria Sand"
+  },
+  {
+    image: "images/zoom/euron1.png",
+    answer: "Euron Greyjoy"
+  },
+  {
+    image: "images/zoom/gendry1.png",
+    answer: "Gendry Baratheon"
+  },
+  {
+    image: "images/zoom/gilly1.png",
+    answer: "Gilly"
+  },
+  {
+    image: "images/zoom/gregor1.png",
+    answer: "Gregor Clegane"
+  },
+  {
+    image: "images/zoom/grenn1.png",
+    answer: "Grenn"
+  },
+  {
+    image: "images/zoom/hodor1.png",
+    answer: "Hodor"
+  },
+  {
+    image: "images/zoom/janos1.png",
+    answer: "Janos Slynt"
+  },
+  {
+    image: "images/zoom/jaqen1.png",
+    answer: "Jaqen H'ghar"
+  },
+  {
+    image: "images/zoom/jeor1.png",
+    answer: "Jeor Mormont"
+  },
+  {
+    image: "images/zoom/joffrey1.png",
+    answer: "Joffrey Baratheon"
+  },
+  {
+    image: "images/zoom/jojen1.png",
+    answer: "Jojen Reed"
+  },
+  {
+    image: "images/zoom/jorah1.png",
+    answer: "Jorah Mormont"
+  },
+  {
+    image: "images/zoom/kevan1.png",
+    answer: "Kevan Lannister"
+  },
+  {
+    image: "images/zoom/lancel1.png",
+    answer: "Lancel Lannister"
+  },
+  {
+    image: "images/zoom/locke1.png",
+    answer: "Locke"
+  },
+  {
+    image: "images/zoom/loras1.png",
+    answer: "Loras Tyrell"
+  },
+  {
+    image: "images/zoom/luwin1.png",
+    answer: "Maestro Luwin"
+  },
+  {
+    image: "images/zoom/lyanna1.png",
+    answer: "Lyanna Mormont"
+  },
+  {
+    image: "images/zoom/lysa1.png",
+    answer: "Lysa Arryn"
+  },
+  {
+    image: "images/zoom/mance1.png",
+    answer: "Mance Rayder"
+  },
+  {
+    image: "images/zoom/margery1.png",
+    answer: "Margery Tyrell"
+  },
+  {
+    image: "images/zoom/meera1.png",
+    answer: "Meera Reed"
+  },
+  {
+    image: "images/zoom/melisandre1.png",
+    answer: "Melisandre"
+  },
+  {
+    image: "images/zoom/missandei1.png",
+    answer: "Missandei"
+  },
+  {
+    image: "images/zoom/night1.png",
+    answer: "Il Re della Notte"
+  },
+  {
+    image: "images/zoom/oberyn1.png",
+    answer: "Oberyn Martell"
+  },
+  {
+    image: "images/zoom/olenna1.png",
+    answer: "Olenna Tyrell"
+  },
+  {
+    image: "images/zoom/olly1.png",
+    answer: "Olly"
+  },
+  {
+    image: "images/zoom/pescenero1.png",
+    answer: "Brynden Tully il 'Pesce Nero'"
+  },
+  {
+    image: "images/zoom/pycelle1.png",
+    answer: "Maestro Pycelle"
+  },
+  {
+    image: "images/zoom/qyburn1.png",
+    answer: "Qyburn"
+  },
+  {
+    image: "images/zoom/ramsay1.png",
+    answer: "Ramsay Bolton"
+  },
+  {
+    image: "images/zoom/renly1.png",
+    answer: "Renly Baratheon"
+  },
+  {
+    image: "images/zoom/rickon1.png",
+    answer: "Rickon Stark"
+  },
+  {
+    image: "images/zoom/rodrik.png",
+    answer: "Rodrik Cassel"
+  },
+  {
+    image: "images/zoom/roose1.png",
+    answer: "Roose Bolton"
+  },
+  {
+    image: "images/zoom/sam1.png",
+    answer: "Samwell Tarly"
+  },
+  {
+    image: "images/zoom/sandor1.png",
+    answer: "Sandor Clegane"
+  },
+  {
+    image: "images/zoom/sansa1.png",
+    answer: "Sansa Stark"
+  },
+  {
+    image: "images/zoom/shireen1.png",
+    answer: "Shireen Baratheon"
+  },
+  {
+    image: "images/zoom/sparrow1.png",
+    answer: "Alto Passero"
+  },
+  {
+    image: "images/zoom/stannis1.png",
+    answer: "Stannis Baratheon"
+  },
+  {
+    image: "images/zoom/syrio1.png",
+    answer: "Syrio Forel"
+  },
+  {
+    image: "images/zoom/theon1.png",
+    answer: "Theon Greyjoy"
+  },
+  {
+    image: "images/zoom/thoros1.png",
+    answer: "Thoros di Myr"
+  },
+  {
+    image: "images/zoom/tommen1.png",
+    answer: "Tommen Baratheon"
+  },
+  {
+    image: "images/zoom/tormund1.png",
+    answer: "Tormund Veleno-dei-Giganti"
+  },
+  {
+    image: "images/zoom/varys1.png",
+    answer: "Varys"
+  },
+  {
+    image: "images/zoom/verme1.png",
+    answer: "Verme Grigio"
+  },
+  {
+    image: "images/zoom/viserys1.png",
+    answer: "Viserys Targaryen"
+  },
+  {
+    image: "images/zoom/walder1.png",
+    answer: "Walder Frey"
+  },
+  {
+    image: "images/zoom/yara1.png",
+    answer: "Yara/Asha Greyjoy"
+  },
+  {
+    image: "images/zoom/ygritte1.png",
+    answer: "Ygritte"
+  },
+  {
+    image: "images/zoom/yoren1.png",
+    answer: "Yoren"
   }
 ];
 
