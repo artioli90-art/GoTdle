74972612a5ab4d048419535a9bcf59520c117b39
artioli90-art <artioli90@gmail.com>
2026-06-18 11:38:51 +0200

    inseriti i primi 52 valori

--- map.js
+++ map.js
@@ -1,1 +1,366 @@
-
+const locations = [
+{
+  name: "forte terrore",
+  region: "",
+  house: "",
+  x: 205,
+  y: 115
+},
+{
+  name: "karhold",
+  region: "",
+  house: "",
+  x: 239,
+  y: 94
+},
+{
+  name: "ultimo focolare",
+  region: "",
+  house: "",
+  x: 200,
+  y: 83
+},
+{
+  name: "deepwood motte",
+  region: "",
+  house: "",
+  x: 118,
+  y: 100
+},
+{
+  name: "isola dell'orso",
+  region: "",
+  house: "",
+  x: 107,
+  y: 71
+},
+{
+  name: "pugno dei primi uomini",
+  region: "",
+  house: "",
+  x: 147,
+  y: 15
+},
+{
+  name: "castello di craster",
+  region: "",
+  house: "",
+  x: 180,
+  y: 30
+},
+{
+  name: "aspra dimora",
+  region: "",
+  house: "",
+  x: 216,
+  y: 16
+},
+{
+  name: "torre delle ombre",
+  region: "",
+  house: "",
+  x: 167,
+  y: 43
+},
+{
+  name: "castello nero",
+  region: "",
+  house: "",
+  x: 188,
+  y: 45
+},
+{
+  name: "piazza di torrhen",
+  region: "",
+  house: "",
+  x: 121,
+  y: 143
+},
+{
+  name: "grande inverno",
+  region: "",
+  house: "",
+  x: 154,
+  y: 127
+},
+{
+  name: "porto bianco",
+  region: "",
+  house: "",
+  x: 180,
+  y: 174
+},
+{
+  name: "moat cailin",
+  region: "",
+  house: "",
+  x: 157,
+  y: 184
+},
+{
+  name: "delta delle acque",
+  region: "",
+  house: "",
+  x: 135,
+  y: 286
+},
+{
+  name: "castel granito",
+  region: "",
+  house: "",
+  x: 77,
+  y: 318
+},
+{
+  name: "lannisport",
+  region: "",
+  house: "",
+  x: 77,
+  y: 326
+},
+{
+  name: "pyke",
+  region: "",
+  house: "",
+  x: 80,
+  y: 266
+},
+{
+  name: "forte clegane",
+  region: "",
+  house: "",
+  x: 80,
+  y: 329
+},
+{
+  name: "harrenhal",
+  region: "",
+  house: "",
+  x: 174,
+  y: 296
+},
+{
+  name: "nido dell'aquila",
+  region: "",
+  house: "",
+  x: 215,
+  y: 260
+},
+{
+  name: "porta insanguinata",
+  region: "",
+  house: "",
+  x: 205,
+  y: 265
+},
+{
+  name: "maidenpool",
+  region: "",
+  house: "",
+  x: 207,
+  y: 302
+},
+{
+  name: "roccia del drago",
+  region: "",
+  house: "",
+  x: 244,
+  y: 314
+},
+{
+  name: "tarth",
+  region: "",
+  house: "",
+  x: 255,
+  y: 378
+},
+{
+  name: "capo tempesta",
+  region: "",
+  house: "",
+  x: 227,
+  y: 384
+},
+{
+  name: "sala dell'estate",
+  region: "",
+  house: "",
+  x: 191,
+  y: 387
+},
+{
+  name: "rostro del grifone",
+  region: "",
+  house: "",
+  x: 219,
+  y: 392
+},
+{
+  name: "lancia del sole",
+  region: "",
+  house: "",
+  x: 249,
+  y: 465
+},
+{
+  name: "arbor",
+  region: "",
+  house: "",
+  x: 57,
+  y: 471
+},
+{
+  name: "collina del corno",
+  region: "",
+  house: "",
+  x: 114,
+  y: 414
+},
+{
+  name: "vecchia citta",
+  region: "",
+  house: "",
+  x: 75,
+  y: 434
+},
+{
+  name: "stelle al tramonto",
+  region: "",
+  house: "",
+  x: 115,
+  y: 448
+},
+{
+  name: "approdo del re",
+  region: "",
+  house: "",
+  x: 208,
+  y: 337
+},
+{
+  name: "baia delle acque nere",
+  region: "",
+  house: "",
+  x: 228,
+  y: 330
+},
+{
+  name: "le stepstone",
+  region: "",
+  house: "",
+  x: 280,
+  y: 435
+},
+{
+  name: "collina fantasma",
+  region: "",
+  house: "",
+  x: 244,
+  y: 446
+},
+{
+  name: "alto giardino",
+  region: "",
+  house: "",
+  x: 107,
+  y: 399
+},
+{
+  name: "ashford",
+  region: "",
+  house: "",
+  x: 144,
+  y: 393
+},
+{
+  name: "le dita",
+  region: "",
+  house: "",
+  x: 242,
+  y: 222
+},
+{
+  name: "braavos",
+  region: "",
+  house: "",
+  x: 314,
+  y: 231
+},
+{
+  name: "pentos",
+  region: "",
+  house: "",
+  x: 314,
+  y: 341
+},
+{
+  name: "volantis",
+  region: "",
+  house: "",
+  x: 428,
+  y: 466
+},
+{
+  name: "valyria",
+  region: "",
+  house: "",
+  x: 488,
+  y: 560
+},
+{
+  name: "lys",
+  region: "",
+  house: "",
+  x: 320,
+  y: 466
+},
+{
+  name: "tyrosh",
+  region: "",
+  house: "",
+  x: 291,
+  y: 411
+},
+{
+  name: "myr",
+  region: "",
+  house: "",
+  x: 336,
+  y: 408
+},
+{
+  name: "vaes dothrak",
+  region: "",
+  house: "",
+  x: 743,
+  y: 311
+},
+{
+  name: "mereen",
+  region: "",
+  house: "",
+  x: 623,
+  y: 436
+},
+{
+  name: "astapor",
+  region: "",
+  house: "",
+  x: 596,
+  y: 493
+},
+{
+  name: "yunkai",
+  region: "",
+  house: "",
+  x: 605,
+  y: 451
+},
+{
+  name: "qarth",
+  region: "",
+  house: "",
+  x: 802,
+  y: 537
+}
+];
